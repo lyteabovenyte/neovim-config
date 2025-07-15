a9ac69ca9f5e11438a6d346d63ed9f196af1cf72
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-15 07:41:09 +0330

    error checking and <leader>cc for cargo check command

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- ~/.config/nvim/init事例
+-- ~/.config/nvim/init.lua
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -12,6 +12,14 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.keymap.set("n", "<CR>", "<CR>", { buffer = true, noremap = true, silent = true })
+  end,
+})
+
 vim.opt.rtp:prepend(lazypath)
 
 -- Ensure statusline is always visible
@@ -20,6 +28,7 @@ vim.o.laststatus = 2
 -- Enable line numbers and relative line numbers
 vim.o.number = true
 vim.o.relativenumber = true
+vim.g.open_command = nil
 
 -- Set leader key for vim-fugitive keymaps
 vim.g.mapleader = " "
@@ -49,3 +58,22 @@ vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { noremap = true, sile
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true, desc = "Next diagnostic" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true, desc = "Previous diagnostic" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Show diagnostic details" })
+
+-- Keymap to run cargo check and populate quickfix list
+vim.keymap.set("n", "<leader>cc", ":lua RunCargoCheck()<CR>", { noremap = true, silent = true, desc = "Run cargo check" })
+
+-- Keymaps for navigating quickfix list (cargo check errors)
+vim.keymap.set("n", "<leader>]e", ":try | cnext | wincmd p | catch | cfirst | wincmd p | endtry<CR>", { noremap = true, silent = true, desc = "Next cargo check error" })
+vim.keymap.set("n", "<leader>[e", ":try | cprev | wincmd p | catch | clast | wincmd p | endtry<CR>", { noremap = true, silent = true, desc = "Previous cargo check error" })
+
+-- Function to run cargo check and populate quickfix list
+function RunCargoCheck()
+  vim.opt.errorformat = "%f:%l:%c: %t%*[^:]: %m,%f:%l:%c %m,%-G%.%#"
+  vim.cmd("cd %:p:h")
+  local output = vim.fn.systemlist("cargo check")
+  vim.fn.setqflist({}, "r", { title = "Cargo Check", lines = output })
+  vim.cmd("copen | cfirst")
+end
+
+-- Configure makeprg for cargo check
+vim.opt.makeprg = "cargo"
--- lua/amir/rust.lua
+++ lua/amir/rust.lua
@@ -3,6 +3,14 @@ local rt = require("rust-tools")
 
 rt.setup({
   server = {
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = true,
+        check = {
+          command = "check",
+        }
+      },
+    },
     on_attach = function(_, bufnr)
       vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
       vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
